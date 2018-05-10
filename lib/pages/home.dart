461ed7165b7692dfecb5c3be40992f5f44cee250
Tiago Ornelas <tiago@front.no>
2018-05-10 17:21:36 +0100

    Added video_player plugin. First time adding a library in flutter :rocket:

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../utils/app_utils.dart';
+import 'package:video_player/video_player.dart';
+
 
 class Home extends StatefulWidget {
   @override
@@ -8,13 +10,42 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
+  VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    debugPrint("a: ");
+    super.initState();
+    _controller = new VideoPlayerController.network('http://live.impresa.pt/live/sic/sic.m3u8');
+//    _controller = new VideoPlayerController.network("http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_20mb.mp4");
+    _controller.initialize();
+    _controller.play();
+
+    _controller.addListener(() {
+      setState(() {});
+
+      debugPrint("isPlaying? " + _controller.value.isPlaying.toString());
+      debugPrint("position " + _controller.value.position.toString());
+      debugPrint("duration " + _controller.value.duration.toString());
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
 
     if (AppUtils.isLandscape(context)){
         return new Text("home landscape");
     }else{
-      return new Text("home portrait");
+
+      return new Column(children: <Widget>[
+
+        new Row(children: <Widget>[
+          new IconButton(icon: new Icon(Icons.play_arrow), onPressed: () => _controller.play()),
+          new IconButton(icon: new Icon(Icons.pause), onPressed: () => _controller.pause()),
+        ]),
+        new AspectRatio(aspectRatio: 1280 / 720,
+        child: new VideoPlayer(_controller))
+      ]);
     }
   }
 }
